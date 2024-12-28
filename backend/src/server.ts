f28cafa54d525d171a78de97c566f5057c60a398
Siddharth Gupta <siddharthgupta1603@gmail.com>
2024-12-28 20:17:24 +0530

    changes are done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './config/database';
 import enrollmentRoutes from './routes/enrollmentRoutes';
 import adminRoutes from './routes/adminRoutes';
+import path from "path"
 
 dotenv.config();
 
@@ -15,12 +16,22 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+const __dirname=path.resolve();
 
 // Routes
 app.use('/api/enrollments', enrollmentRoutes);
 app.use('/api/admin/login', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../my-react-app/dist")))
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname,"../my-react-app","dist","index.html"))
+  })
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
